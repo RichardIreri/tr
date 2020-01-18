202205a05848af985a76f5352b7f32b94b4ae81a
Ritchie <richardireri19@gmail.com>
2020-01-18 14:42:38 +0300

    requirements

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ritchie/flasky/venv"
+set -gx VIRTUAL_ENV "/home/ritchie/Flasky/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
